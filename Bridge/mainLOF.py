efeb2ec57ee5f3ad8aaf0885f326d0747ecaa729
FrancescoBellei29 <70749217+FrancescoBellei29@users.noreply.github.com>
2021-03-31 11:48:29 +0200

    Added time of day anomaly detection in mainLOF.py

--- Bridge/mainLOF.py
+++ Bridge/mainLOF.py
@@ -16,7 +16,7 @@ geographic_coordinates = [
  (44.773674, 10.874135),
  (44.773640, 10.874202)]
 
-lof = LOF("geodesic_distance", geographic_coordinates)
+lof = LOF("geodesic_distance", geographic_coordinates, normalize=True)
 
 # Esempi di outliers: punti presi a una casa di distanza, tre case di distanza, una via parallela
 for instance in [[44.773564, 10.874451], [44.773406, 10.874851], [44.774288, 10.874526]]:
@@ -31,4 +31,33 @@ for instance in [[44.773564, 10.874451], [44.773406, 10.874851], [44.774288, 10.
 x,y = zip(*geographic_coordinates)
 p.scatter(x,y, 9, color="#0000FF")
 
+p.show()
+
+# Calcolo lof per gli orari (lasciato due assi per non cambiare il codice, la coordinata y è sempre 0)
+opening_hours_in_minutes = [
+ (480, 0), #8:00
+ (482, 0), #8:02
+ (485, 0), #8:05
+ (474, 0), #7:54
+ (477, 0), #7:57
+ (1110, 0), #18:30
+ (1113, 0), #18:33
+ (1107, 0), #18:27
+ (1105, 0)] #18:25
+
+
+lof = LOF("euclidean_distance", opening_hours_in_minutes, normalize=False)
+
+# Esempi di outliers: ore lontane da quelle inserite sopra
+for instance in [[840, 0], [900, 0], [1320, 0]]:
+    # Per ogni istanza mi faccio tornare il valore di lof, se è >=1 il punto è outlier e lo coloro di rosso
+    # e lo faccio più grande più è ourlier, se è <1 è inlier e lo faccio verde
+    value = lof.local_outlier_factor(3, instance)
+    print(value, instance)
+    color = "#FF0000" if value > 1 else "#00FF00"
+    p.scatter(instance[0], instance[1], color=color, s=(value - 1) ** 2 * 10 + 20)
+
+    # Disegno anche i punti forniti come orari in blu
+x, y = zip(*opening_hours_in_minutes)
+p.scatter(x, y, 9, color="#0000FF")
 p.show()
