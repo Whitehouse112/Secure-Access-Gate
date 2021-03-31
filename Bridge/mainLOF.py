022357e0af5d9ffa017a1821bb9b74dd075a20bc
FrancescoBellei29 <70749217+FrancescoBellei29@users.noreply.github.com>
2021-03-31 11:06:23 +0200

    Updated mainLOF.py

--- Bridge/mainLOF.py
+++ Bridge/mainLOF.py
@@ -1,5 +1,6 @@
 from lof import LOF
 from matplotlib import pyplot as p
+import numpy as np
 
 # Calcolo lof per le coordinate geografiche
 
@@ -19,16 +20,15 @@ lof = LOF("geodesic_distance", geographic_coordinates)
 
 # Esempi di outliers: punti presi a una casa di distanza, tre case di distanza, una via parallela
 for instance in [[44.773564, 10.874451], [44.773406, 10.874851], [44.774288, 10.874526]]:
+    # Per ogni istanza mi faccio tornare il valore di lof, se è >=1 il punto è outlier e lo coloro di rosso
+    # e lo faccio più grande più è ourlier, se è <1 è inlier e lo faccio verde
     value = lof.local_outlier_factor(3, instance)
     print(value, instance)
+    color = "#FF0000" if value > 1 else "#00FF00"
+    p.scatter(instance[0], instance[1], color=color, s=(value - 1) ** 2 * 10 + 20)
 
+# Disegno anche i punti forniti come coordinate del cancello in blu
 x,y = zip(*geographic_coordinates)
 p.scatter(x,y, 9, color="#0000FF")
-
-# Uguale ma per il grafico
-for instance in [[44.773564, 10.874451], [44.773406, 10.874851], [44.774288, 10.874526]]:
-    value = lof.local_outlier_factor(3, instance)
-    color = "#FF0000" if value > 1 else "#00FF00"
-    p.scatter(instance[0], instance[1], color=color, s=(value-1)**2*10+20)
 
 p.show()
