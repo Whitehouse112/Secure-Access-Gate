f65bd616cc0c9d09938ce4687d287c633780df47
FrancescoBellei29 <70749217+FrancescoBellei29@users.noreply.github.com>
2021-03-31 09:00:55 +0200

    Updated mainLOF.py

--- Bridge/mainLOF.py
+++ Bridge/mainLOF.py
@@ -5,20 +5,20 @@ from matplotlib import pyplot as p
 
 # Punti presi a caso nell'intorno di pochi metri di un cancello di una casa random
 geographic_coordinates = [
- (44.77364720217285, 10.874154166188879),
- (44.77365862668815, 10.874135390728647),
- (44.77368623592409, 10.874191717116714),
- (44.77362340109209, 10.874142096251035),
- (44.77363054141734, 10.874144107907751),
- (44.77367909560573, 10.874174953310744),
- (44.77365481851662, 10.874218539206272),
- (44.77367481141427, 10.874135390728647),
- (44.77364050064099, 10.874202428716771)]
+ (44.773647, 10.874154),
+ (44.773658, 10.874135),
+ (44.773686, 10.874191),
+ (44.773623, 10.874142),
+ (44.773630, 10.874144),
+ (44.773679, 10.874174),
+ (44.773654, 10.874218),
+ (44.773674, 10.874135),
+ (44.773640, 10.874202)]
 
 lof = LOF("geodesic_distance", geographic_coordinates)
 
 # Esempi di outliers: punti presi a una casa di distanza, tre case di distanza, una via parallela
-for instance in [[44.77356433519205, 10.87445184529774], [44.77340629559869, 10.87485149443705], [44.77428822057317, 10.87452670388886]]:
+for instance in [[44.773564, 10.874451], [44.773406, 10.874851], [44.774288, 10.874526]]:
     value = lof.local_outlier_factor(3, instance)
     print(value, instance)
 
@@ -26,7 +26,7 @@ x,y = zip(*geographic_coordinates)
 p.scatter(x,y, 9, color="#0000FF")
 
 # Uguale ma per il grafico
-for instance in [[44.77356433519205, 10.87445184529774], [44.77340629559869, 10.87485149443705], [44.77428822057317, 10.87452670388886]]:
+for instance in [[44.773564, 10.874451], [44.773406, 10.874851], [44.774288, 10.874526]]:
     value = lof.local_outlier_factor(3, instance)
     color = "#FF0000" if value > 1 else "#00FF00"
     p.scatter(instance[0], instance[1], color=color, s=(value-1)**2*10+20)
