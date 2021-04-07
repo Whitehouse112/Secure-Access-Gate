2d9b724cec5b09d39583be7fc441ec18783abda6
Troky <Lorenzorosinitroky@live.it>
2021-04-07 22:20:44 +0200

    Changed notidication for testing purposes

--- Backend/notification.py
+++ Backend/notification.py
@@ -1,5 +1,7 @@
 from pyfcm import FCMNotification
 
+DEVICE_TOKEN = "cYgzqik3Sdu88GhM6Kz7FO:APA91bFyrSyqmoM0KoTRkZAxaESVfKOPwttewVuIzFsj_5XyZyWmDLT5Uksi-ert1M4IlMZWK4BPHJyy1azbTkkDHLlhpvuzGhDRP8rZqlX9XaKYdXm3JsAS7LDdDM62T-5CymXw_OHf"
+
 class Notification:
     def __init__(self):
         self.push_service = FCMNotification(api_key="AAAAxL7lTsI:APA91bEsVnNzbVM2cZ2rbkl05xSYf7IVMayZpszta2QiCCqgP-8TvtDMzvkQLuQxPJuAlApXRLQQJMsBzH2a7hg9FTboUMNtAmDQktvdjp-_LNk7x7NfzsiE71ETxSd1OknVmlexum3h")
@@ -12,7 +14,6 @@ class Notification:
         self.push_service.notify_topic_subscribers(topic, message_title=title, message_body=body)
 
     def sendToDevice(self, token, title, body):
-        token = "cYgzqik3Sdu88GhM6Kz7FO:APA91bFyrSyqmoM0KoTRkZAxaESVfKOPwttewVuIzFsj_5XyZyWmDLT5Uksi-ert1M4IlMZWK4BPHJyy1azbTkkDHLlhpvuzGhDRP8rZqlX9XaKYdXm3JsAS7LDdDM62T-5CymXw_OHf"
         #message_title = "From python"
         #message_body = "Content of message sent from python"
         data = {"status":"open", "date":"now"}
@@ -20,4 +21,4 @@ class Notification:
         self.push_service.notify_single_device(token, message_title=title, message_body=body, data_message=data)
 
 if __name__ == "__main__":
-    Notification().sendToDevice("", "Notification from Python", "Text of the nootification")
+    Notification().sendToDevice(DEVICE_TOKEN, "New access attemp", "What action should i undertake?")
