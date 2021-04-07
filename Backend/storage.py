00060592fa265ad5db03b21de582a785e2291a2f
Troky <Lorenzorosinitroky@live.it>
2021-04-07 20:00:39 +0200

    Added storage class for handling the storage of images

---
+++ Backend/storage.py
@@ -0,0 +1,35 @@
+from google.cloud import storage
+
+BUCKET_NAME = 'iot-project'
+
+class Storage:
+    def __init__(self):
+        self.client = storage.Client
+
+    def upload_image(self, file, blob_name):
+        bucket = self.client.bucket(BUCKET_NAME)
+        blob = bucket.blob(blob_name)
+        try:
+            blob.upload_from_file(file)
+        except GoogleCloudError:
+            return 500
+        return 'Success'
+
+    def get_image(self, blob_name):
+        bucket = self.client.bucket(BUCKET_NAME)
+        blob = bucket.blob(blob_name)
+        if blob.exists():
+            return blob.download_to_filename() #TODO: the other one is deprecated, we need to supply a filename
+        else:
+            return None
+
+    def delete_image(self, blob_name):
+        bucket = self.client.bucket(BUCKET_NAME)
+        blob = bucket.blob(blob_name)
+        if blob.exists():
+            blob.delete
+
+    def check_image(self, blob_name):
+        bucket = client.bucket(BUCKET_NAME)
+        blob = bucket.blob(blob_name)
+        return blob.exists()
