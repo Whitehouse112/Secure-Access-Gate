2424482f1c2b00099603fd0b1f67495b0e86cbe4
Troky <Lorenzorosinitroky@live.it>
2021-04-08 10:13:42 +0200

    Added Cloud Pub/Sub

---
+++ Backend/pubsub.py
@@ -0,0 +1,20 @@
+from google.cloud import pubsub_v1
+
+topic_name = 'projects/{project_id}/topics/{topic}'.format(
+    project_id=os.getenv('GOOGLE_CLOUD_PROJECT'),
+    topic='test',  # Set this to something appropriate.
+)
+
+subscription_name = 'projects/{project_id}/subscriptions/{sub}'.format(
+    project_id=os.getenv('GOOGLE_CLOUD_PROJECT'),
+    sub='test-uuid',  # Set this to something appropriate.
+)
+
+def callback(message):
+    print(message.data)
+    message.ack()
+
+with pubsub_v1.SubscriberClient() as subscriber:
+    subscriber.create_subscription(
+        name=subscription_name, topic=topic_name)
+    future = subscriber.subscribe(subscription_name, callback)
